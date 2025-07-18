a5a9285ab2234c502a56aec8ae6b96df237cd43d
Mansour <m_emami21@cs.uni-kl.de>
2025-07-18 18:11:27 +0200

    Movie polymorphism 2

--- 58.Polymorphism/src/Main.java
+++ 58.Polymorphism/src/Main.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Movie theMovie = new Adventure("Star wars");
+       /* Movie theMovie = new Adventure("Star wars");
         theMovie.watchMovie();
 
         Movie theComedy = new Comedy("Star wars");
         theMovie.watchMovie();
 
         Movie theScienceFickshen = new ScienceFiction("Star wars");
+        theMovie.watchMovie();*/
+
+        Movie theMovie = Movie.getMovie("Come" , "Star Wars");
         theMovie.watchMovie();
+
+        Scanner s = new Scanner(System.in);
+        while(true){
+            System.out.print("Enter Type (A for Adventure, C for Comedy, " +
+                            "S for Science Fiction, or Q to quit: ");
+            String type = s.nextLine();
+            if("Qq".contains(type)){
+                break;
+            }
+            System.out.print("Enter Movie Title: ");
+            String title = s.nextLine();
+            Movie theMovie2 = Movie.getMovie(type , title);
+            theMovie2.watchMovie();
+
+        }
+
 
     }
 }
