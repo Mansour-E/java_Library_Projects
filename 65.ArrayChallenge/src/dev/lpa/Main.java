ce33fce0b97b60f200b7a229a7aac1dfb6ca44b4
Mansour <m_emami21@cs.uni-kl.de>
2025-07-25 13:46:34 +0200

    Random ASC Sorted Array

--- 65.ArrayChallenge/src/dev/lpa/Main.java
+++ 65.ArrayChallenge/src/dev/lpa/Main.java
@@ -6,13 +6,18 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
+        int[] unsortedArray = getRandomArray(10);
+        System.out.println(Arrays.toString(unsortedArray));
+    }
+
+    public static int[] getRandomArray(int len){
 
         Random random = new Random();
-        int[] firstArray = new int[10];
+        int[] firstArray = new int[len];
         for (int i = 0; i < firstArray.length; i++) {
             firstArray[i] = random.nextInt(100);
         }
-        System.out.println(Arrays.toString(firstArray));
+
         Arrays.sort(firstArray);
         System.out.println(Arrays.toString(firstArray));
 
@@ -21,8 +26,10 @@ public class Main {
             firstArray[i] = firstArray[firstArray.length - i - 1 ];
             firstArray[firstArray.length - i - 1] = temp;
         }
-        System.out.println(Arrays.toString(firstArray));
-    }
 
-    public static int[] getRandomArray(int length){}
+        System.out.println(Arrays.toString(firstArray));
+
+        return firstArray;
+
+    }
 }
