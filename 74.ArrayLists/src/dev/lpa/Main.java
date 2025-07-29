77dd0c82abe463406daa6070a5c4d4885400c397
Mansour <m_emami21@cs.uni-kl.de>
2025-07-29 18:54:32 +0200

    ArrayList

---
+++ 74.ArrayLists/src/dev/lpa/Main.java
@@ -0,0 +1,42 @@
+package dev.lpa;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+record GroceryItem(String name , String type, int count){
+
+    public GroceryItem(String name ){
+        this(name , "DAIRY" , 1);
+    }
+}
+
+public class Main{
+
+    public static void main(String[] args) {
+
+        //pass auf , fehler
+        Object[] groceryArray = new Object[3];
+        groceryArray[0] = new GroceryItem("milk");
+        groceryArray[1] = new GroceryItem("apples" , "PRODUCE" , 6);
+        groceryArray[2] = "5 Oranges";
+        System.out.println(Arrays.toString(groceryArray));
+
+        //die korrekte version
+        GroceryItem[] groceryyArray = new GroceryItem[3];
+        groceryyArray[0] = new GroceryItem("milk");
+        groceryyArray[1] = new GroceryItem("apples" , "PRODUCE" , 6);
+        groceryyArray[2] = new GroceryItem("Oranges" , "PROODUCE" , 5);
+        System.out.println(Arrays.toString(groceryyArray));
+
+        //false
+        ArrayList objectList = new ArrayList();
+        objectList.add(new GroceryItem("Butter"));
+        objectList.add("yogurt");
+
+        //true
+        ArrayList<GroceryItem> groceryList = new ArrayList<GroceryItem>();
+        groceryList.add(new GroceryItem("Butter"));
+        //groceryList.add("yogurt"); geht nicht mehr weil die type ist GroceryItem
+
+    }
+}
