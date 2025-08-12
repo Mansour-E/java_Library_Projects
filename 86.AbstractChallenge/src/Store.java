4b8c3c8689699714ba39e083de47203d0ce76121
Mansour <m_emami21@cs.uni-kl.de>
2025-08-12 16:51:37 +0200

    ArtObject Finished

--- 86.AbstractChallenge/src/Store.java
+++ 86.AbstractChallenge/src/Store.java
@@ -14,6 +14,13 @@ public class Store {
 
         listProduct();
 
+        System.out.println("\nOrder 1");
+        var order1 = new ArrayList<OrderItem>();
+        addItemToOrder(order1, 1, 2);
+        addItemToOrder(order1, 0, 1);
+        printOrder(order1);
+
+
     }
 
     public static void listProduct(){
@@ -24,5 +31,21 @@ public class Store {
             item.showDetails();
 
         }
+    }
+
+    public static void addItemToOrder(ArrayList<OrderItem> order, int orderIndex, int quantity){
+
+        order.add(new OrderItem(quantity, storeProducts.get(orderIndex)));
+
+    }
+
+    public static void printOrder(ArrayList<OrderItem> order){
+
+        double salesTotal = 0;
+        for (var item : order){
+            item.product().printPricedItem(item.quantity());
+            salesTotal += item.product().getSalesPrice(item.quantity());
+        }
+        System.out.printf("Sales Total = $%6.2f %n", salesTotal);
     }
 }
