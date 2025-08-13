15697f86bf9589663580d141e900cdb84d3fd0f2
Mansour <m_emami21@cs.uni-kl.de>
2025-08-13 16:00:20 +0200

    FlightStages

--- 87.Interfaces/src/info/emami/Animal.java
+++ 87.Interfaces/src/info/emami/Animal.java
@@ -6,6 +6,10 @@ enum FlightStages implements Trackable{GROUNDED, LAUNCH, CRUISE, DATA_COLLECTION
     @Override
     public void track() {
 
+        if (this != GROUNDED) {
+
+            System.out.println("Monitoring " + this);
+        }
     }
 }
 
