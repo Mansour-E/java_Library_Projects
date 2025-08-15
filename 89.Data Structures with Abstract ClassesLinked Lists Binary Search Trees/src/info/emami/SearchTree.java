6e459076fc3d0737b834f24fd4d53d64537164bf
Mansour <m_emami21@cs.uni-kl.de>
2025-08-15 13:46:00 +0200

    LinkedList codiert

--- 89.Data Structures with Abstract ClassesLinked Lists Binary Search Trees/src/info/emami/SearchTree.java
+++ 89.Data Structures with Abstract ClassesLinked Lists Binary Search Trees/src/info/emami/SearchTree.java
@@ -2,20 +2,45 @@ package info.emami;
 
 public class SearchTree implements NodeList{
 
+    private ListItem root = null;
+
+    public SearchTree(ListItem root) {
+        this.root = root;
+    }
 
     @Override
     public ListItem getRoot() {
-        return null;
+        return this.root;
     }
 
     @Override
     public boolean addItem(ListItem item) {
-        return false;
+
+        if(this.root == null ){
+
+            this.root = item;
+            return true;
+        }
+
+        ListItem currentItem = this.root;
+        while (currentItem != null) {
+
+            int comparison = currentItem.compareTo(item);
+            if (comparison < 0) {// newItem is greater, move right if possible
+
+
+            }
+        }
     }
 
     @Override
     public boolean removeItem(ListItem item) {
         return false;
+    }
+
+    private void performRemoval(ListItem item1, ListItem item2){
+
+
     }
 
     @Override
