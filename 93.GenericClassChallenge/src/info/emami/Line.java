1fbd6b5f748e21d6e355f7b05bc8baec6b8553e1
Mansour <mansuurem@gmail.com>
2025-08-18 19:17:00 +0200

    Lin class is finished

--- 93.GenericClassChallenge/src/info/emami/Line.java
+++ 93.GenericClassChallenge/src/info/emami/Line.java
@@ -1,20 +1,26 @@
 package info.emami;
 
+import java.util.Arrays;
+
 public abstract class Line implements Mappable{
 
     protected double[][] locations;
 
-    public Line(double[][] locations) {
+    public Line(String... locations) {
         this.locations = new double[locations.length][];
+        int i = 0;
+        for(var l : locations){
+            this.locations[i++] = Mappable.stringToLatLon(l); // i love that . this method will take them as String then put them in that Array
+        }
     }
 
-    private String location(){
+    private String locations(){
 
-        return null;
+        return Arrays.deepToString(locations);
     }
 
     @Override
     public void render() {
-
+        System.out.println("Render " + this + " as LINE (" + locations() + ")");
     }
 }
