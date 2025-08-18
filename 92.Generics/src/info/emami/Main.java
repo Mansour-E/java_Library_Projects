8a008efbded764b8a70e378b3c99fa3d8bbf1c01
Mansour <mansuurem@gmail.com>
2025-08-18 14:11:41 +0200

    Generics BaseballTeam class and BeseballPlayer record and Player interfaces

---
+++ 92.Generics/src/info/emami/Main.java
@@ -0,0 +1,33 @@
+package info.emami;
+
+interface Player{}
+
+record BeseballPlayer(String name, String position){}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BaseballTeam phillies = new BaseballTeam("Philadelphia Phillies");
+        BaseballTeam astros = new BaseballTeam("Houston Astros");
+        scoreResult(phillies, 3, astros, 5);
+
+        var harper = new BeseballPlayer("B Harper", "Right Fielder");
+        var marsh = new BeseballPlayer("B Marsh", "Right Fielder");
+        phillies.addTeamMember(harper);
+        phillies.addTeamMember(marsh);
+        phillies.listTeamMember();
+
+
+    }
+
+    public static void scoreResult(BaseballTeam team1, int t1_score,
+                                   BaseballTeam team2, int t2_score){
+
+        String message = team1.setScore(t1_score, t2_score);
+        team2.setScore(t2_score, t1_score);
+        System.out.printf("%s %s %s %n", team1, message, team2);
+
+
+    }
+}
