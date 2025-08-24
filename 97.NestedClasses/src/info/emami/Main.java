50a547e63ba676018c839c86283943402777fae4
Mansour Emami <mansuurem@gmail.com>
2025-08-24 15:23:43 +0200

    StoreEmployee and StoreComparator . generic nested classes

--- 97.NestedClasses/src/info/emami/Main.java
+++ 97.NestedClasses/src/info/emami/Main.java
@@ -1,6 +1,7 @@
 package info.emami;
 
 import info.emami.domain.Employee;
+import info.emami.domain.StoreEmployee;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +26,26 @@ public class Main {
         for (Employee e : employees) {
             System.out.println(e);
         }
+
+        System.out.println("Store Members");
+
+        List<StoreEmployee> storeEmployees = new ArrayList<>(List.of(
+                new StoreEmployee(10015, "Meg", 2019, "Target"),
+                new StoreEmployee(10515, "Joe", 2021, "Walmart"),
+                new StoreEmployee(10105, "Tom",2020, "Macys"),
+                new StoreEmployee(10215, "Marty", 2018,"Walmart")
+
+        ));
+
+        var genericEmployee = new StoreEmployee();
+        var comparator = new StoreEmployee().new StoreComparator<>();
+        storeEmployees.sort(comparator);
+
+        for(StoreEmployee e : storeEmployees){
+            System.out.println(e);
+        }
+
+
 
     }
 }
