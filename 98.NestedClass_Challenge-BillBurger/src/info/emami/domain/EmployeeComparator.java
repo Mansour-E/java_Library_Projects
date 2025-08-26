485ac392ed79f35a85ab5443c2d7fd342be2866f
Mansour Emami <mansuurem@gmail.com>
2025-08-26 17:12:52 +0200

    updated Eployee and comparator and store Employee

--- 98.NestedClass_Challenge-BillBurger/src/info/emami/domain/EmployeeComparator.java
+++ 98.NestedClass_Challenge-BillBurger/src/info/emami/domain/EmployeeComparator.java
@@ -1,4 +1,11 @@
 package info.emami.domain;
 
-public class EmployeeComparator {
+import java.util.Comparator;
+
+public class EmployeeComparator <T extends Employee> implements Comparator<Employee> {
+
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
 }
