e91bda9f61acadfbc31cf5c7796e983b0b76cd93
Mansour Emami <mansuurem@gmail.com>
2025-09-01 18:19:45 +0200

    lambda Expression

--- 103.LambdaExpressions/src/info/emami/Main.java
+++ 103.LambdaExpressions/src/info/emami/Main.java
@@ -5,6 +5,30 @@ import java.util.List;
 
 public class Main {
 
-    List<String> list = new ArrayList<>(List.of("alpha", "bravo", "charlie", "delta"));
+    public static void main(String[] args) {
 
+        List<String> list = new ArrayList<>(List.of("alpha", "bravo", "charlie", "delta"));
+
+        for(String s : list){
+            System.out.println(s);
+        }
+
+        System.out.println("--------");
+        list.forEach((var s) -> System.out.println(s));
+        list.forEach((var s) -> {
+            System.out.println("Hello");
+            System.out.println(s);
+
+            System.out.println("--------");
+            String prefix = "nato";
+            list.forEach((var myString) -> {
+                char first = myString.charAt(0);
+                System.out.println(prefix + " " +myString + " means " + first);
+            });
+
+        });
+
+
+    }
 }
+
