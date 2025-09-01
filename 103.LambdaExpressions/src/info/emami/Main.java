968a9fb3f01c6b577cfda0cd6948f82b63d4a824
Mansour Emami <mansuurem@gmail.com>
2025-09-01 18:03:42 +0200

    lambda Expression

---
+++ 103.LambdaExpressions/src/info/emami/Main.java
@@ -0,0 +1,10 @@
+package info.emami;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    List<String> list = new ArrayList<>(List.of("alpha", "bravo", "charlie", "delta"));
+
+}
