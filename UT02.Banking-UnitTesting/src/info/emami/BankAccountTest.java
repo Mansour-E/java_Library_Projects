303ff70ebce19696d633efaffe031e5933513d08
Mansour Emami <mansuurem@gmail.com>
2025-09-09 18:20:04 +0200

    create BankAccountTest and setting up that junit4

---
+++ UT02.Banking-UnitTesting/src/info/emami/BankAccountTest.java
@@ -0,0 +1,21 @@
+package info.emami;
+
+import static org.junit.Assert.*;
+
+public class BankAccountTest {
+
+    @org.junit.Test
+    public void deposit() {
+        fail("this test has yet to be implemented");
+    }
+
+    @org.junit.Test
+    public void withdraw() {
+        fail("this test has yet to be implemented");
+    }
+
+    @org.junit.Test
+    public void getBalance() {
+        fail("this test has yet to be implemented");
+    }
+}
