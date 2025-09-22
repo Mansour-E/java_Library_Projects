773ea38755da3e8017d8c2a0c37661f39581546a
Mansour Emami <mansuurem@gmail.com>
2025-09-22 16:43:56 +0200

    population

--- 105.LambdaExpressionChallenge/src/info/emami/Main.java
+++ 105.LambdaExpressionChallenge/src/info/emami/Main.java
@@ -1,8 +1,15 @@
 package info.emami;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        String[] names = new String[]{"Anna", "Tomas", "Bob", "jack" , "Yannic"};
+        ArrayList<String> namess = new ArrayList<>(List.of("Anna", "Tomas", "Bob", "jack" , "Yannic"));
+
 
     }
 }
