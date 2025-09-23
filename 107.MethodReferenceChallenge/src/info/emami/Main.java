c2fc195669df1c85c056b82ed5f4fc678115b854
Mansour Emami <mansuurem@gmail.com>
2025-09-23 17:15:34 +0200

    first step

--- 107.MethodReferenceChallenge/src/info/emami/Main.java
+++ 107.MethodReferenceChallenge/src/info/emami/Main.java
@@ -1,5 +1,36 @@
 package info.emami;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
 
+    public static Random random = new Random();
+
+    public static void main(String[] args) {
+
+        String[] names = new String[]{"Anna", "Bob", "Carol", "David" , "Ed", "Fred", "Gary"};
+
+        Arrays.setAll(names, i -> names[i].toUpperCase());
+
+        List<String> list = Arrays.asList(names);
+        list.replaceAll(s -> s+= " " + getRandomChar('A','B') + "." + getReversedName(s));
+        System.out.println(Arrays.toString(names));
+
+
+
+
+
+    }
+
+    public static char getRandomChar(char startChar, char endChar){
+
+        return (char) random.nextInt((int) startChar, (int) endChar);
+    }
+
+    public static String getReversedName(String firstName){
+
+        return new StringBuilder(firstName).reverse().toString();
+    }
 }
